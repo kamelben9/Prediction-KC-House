8309247891c72560a1306cbea7c24527efd3d7c5
kamel <kamelben9@gmail.com>
2022-08-11 09:05:47 +0200

    version finale

--- streamlitkch.py
+++ streamlitkch.py
@@ -40,7 +40,7 @@ elif waterfront == "Non":
     waterfront = 0
 
 
-condition = st.slider("Condition du bien (Sur une échelle de 1 a 10)", 0, 10)
+condition = st.slider("Condition du bien (Sur une échelle de 1 a 10)", 0, 5)
 
 grade = st.slider("Grade du bien (Sur une échelle de 1 a 10)", 0, 10)
 
@@ -48,7 +48,16 @@ yr_built = st.number_input("Année de construction")
 
 yr_renovated = st.number_input("Année de rénovation")
 
-zipcode = st.text_input("Code postal")
+zipcode = st.selectbox("Code postal", ('98178', '98125', '98028', '98136', '98074', '98053', '98003',
+       '98198', '98146', '98038', '98007', '98115', '98107', '98126',
+       '98019', '98103', '98002', '98133', '98040', '98092', '98030',
+       '98119', '98112', '98052', '98027', '98117', '98058', '98001',
+       '98056', '98166', '98023', '98070', '98148', '98105', '98042',
+       '98008', '98059', '98122', '98144', '98004', '98005', '98034',
+       '98075', '98116', '98010', '98118', '98199', '98032', '98045',
+       '98102', '98077', '98108', '98168', '98177', '98065', '98029',
+       '98006', '98109', '98022', '98033', '98155', '98024', '98011',
+       '98031', '98106', '98072', '98188', '98014', '98055', '98039'))
 
 sqft_liv15 = st.slider("Moyenne de la surface des biens des 15 plus proches voisins (en square feet)", 0, 10000)
 
@@ -65,7 +74,6 @@ liste_col = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
 
 if st.button("Démarrez l'estimation"):
     df = pd.DataFrame(np.array(liste).reshape(1, -1),columns = liste_col)
-    st.write(liste)
     st.write(pickle_model_lr.predict(df))
     price = int(pickle_model_lr.predict(df))
     st.success("{}$".format(price))
